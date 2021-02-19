e2b79322e3b1f9b5ec4c54abcf8df021d9ae03bd
branmar97 <brandonmar97@gmail.com>
2021-02-18 19:44:43 -0500

    Add location and weather containers

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ const api = {
 }
 
 function App() {
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", 
+    "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${month} ${date}, ${year}`
+  }
+
   return (
     <div className="app">
       <main>
@@ -19,7 +32,16 @@ function App() {
         <div>
           <div className='location-box'>
             <div className='location'>New York City, USA</div>
-            <div className='date'>{dateBuilder(new(Date()))}</div>
+            <div className='date'>{dateBuilder(new Date())}</div>
+          </div>
+
+          <div className='weather-box'>
+            <div className='temp'>
+              15°
+            </div>
+            <div className='weather'>
+              Sunny
+            </div>
           </div>
         </div>
       </main>
