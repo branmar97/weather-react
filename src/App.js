6076d8e65ff8c6daf06b7b2a1d9b40635d02b655
branmar97 <brandonmar97@gmail.com>
2021-02-18 20:03:03 -0500

    Add handler for API requests

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 const api = {
   key: '038c8f378edc18025c2f0ec0d424c17f',
   base: 'https://api.openweathermap.org/data/2.5/'
 }
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
+    if (evt.key === 'Enter') {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => setWeather(result));
+    }
+  }
+
   const dateBuilder = (d) => {
     let months = ["January", "February", "March", "April", "May", "June", "July", 
     "August", "September", "October", "November", "December"];
