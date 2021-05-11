9d34886e589ef9d41579846ea028943a20e2a936
branmar97 <brandonmar97@gmail.com>
2021-05-11 10:39:35 -0400

    add card data styling

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,12 +1,11 @@
 const Card = ({ data, zipcode }) => {
 
     return (
-        <div>
-            <h1>Weather in "{zipcode}"</h1>
-            <p>{data.weather[0].description}</p>
-            <h2>{Math.floor(data.main.temp)}°</h2>
-            <span>H:{Math.floor(data.main.temp_max)}° L:{Math.floor(data.main.temp_min)}°</span>
-            <span>Feels like {Math.floor(data.main.feels_like)}°</span>
+        <div className="text-white">
+            <h1 className="text-2xl drop-shadow-lg">Weather in "{zipcode}"</h1>
+            <small className="capitalize">{data.weather[0].description}</small>
+            <h2 className="text-6xl font-bold">{Math.floor(data.main.temp)}°</h2>
+            <small>Feels like {Math.floor(data.main.feels_like)}°</small>
         </div>
     )
 }
