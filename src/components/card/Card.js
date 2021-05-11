78036685468d6c4d4d8e0153070195c05b5c78ff
branmar97 <brandonmar97@gmail.com>
2021-05-10 20:12:43 -0400

    render weather data

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,8 +1,12 @@
-const Card = ({ weather }) => {
+const Card = ({ data, zipcode }) => {
 
     return (
         <div>
-            
+            <h1>Weather in "{zipcode}"</h1>
+            <p>{data.weather[0].description}</p>
+            <h2>{Math.floor(data.main.temp)}°</h2>
+            <span>H:{Math.floor(data.main.temp_max)}° L:{Math.floor(data.main.temp_min)}°</span>
+            <span>Feels like {Math.floor(data.main.feels_like)}°</span>
         </div>
     )
 }
